94a2f4434ce624a949f11a4d891f1cade3cb0b46
TheMasterDodo <Chang-Syuan@Chang-Syuans-MacBook-Pro.local>
2017-02-23 13:15:46 -0500

    mp regen for luminous added

--- fwtcsv/index.js
+++ fwtcsv/index.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const csv = require('csv');
 const JSONStream = require('JSONStream');
 
-const inputFile = 'FWTHeroStats.csv';
-const outputFile = 'FWTHeroStats.json';
+const inputFile = 'High Schools (USA MA) - Schools.csv';
+const outputFile = 'High Schools (USA MA) - Schools.json';
 
 const input = fs.createReadStream(inputFile);
 const output = fs.createWriteStream(outputFile);
 
 input.pipe(csv.parse({
-  delimiter: ';',
+  delimiter: ',',
   columns: true
 })).pipe(JSONStream.stringify()).pipe(output);
