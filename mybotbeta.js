19a8f0ca9cbaaa036e6d32039963be91097c3781
TheMasterDodo <Chang-Syuan@Chang-Syuans-MacBook-Pro.local>
2017-04-06 15:43:15 -0400

    minor change

--- mybotbeta.js
+++ mybotbeta.js
@@ -16,7 +16,7 @@ const heroSkillTable = require(path.join(launchLocation, "Data", "FWTHeroSkills.
 const triviaTable = require(path.join(launchLocation, "Data", "FWTTrivia.json"));
 const flagNames = ["confusion", "charm", "stun", "taunt", "disarm", "immobilize", "decrease movement", "dot", "mp burn", "skill cost", "defense ignore", "defense ignoring damage", "weakening", "buff removal", "hp% damage", "defense decrease", "attack decrease", "hp drain", "mastery decrease", "instant death", "decrease crit rate", "push/pull/switch", "passive attack", "seal", "sleep", "melee", "ranged"];
 
-sql.open(path.join(launchLocation, "Data", "scores.sqlite"));
+sql.open(path.join(launchLocation, "scores.sqlite"));
 var triviaChannels = new Set([]);
 var triviaLastQuestion = 0;
 const logger = new Logger(config.noLogs);
