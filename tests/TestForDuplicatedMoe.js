3fc1c4122d41759dec63b3ff28fcc9eb1df5f114
The Master Dodo <william.wu1219@gmail.com>
2017-07-11 12:10:41 -0400

    fixed test to log properly

--- tests/TestForDuplicatedMoe.js
+++ tests/TestForDuplicatedMoe.js
@@ -2,10 +2,13 @@ const path = require("path");
 const launchLocation = __dirname;
 const moe = require(path.join(launchLocation.slice(0, 37), "src", "Moe.json"));
 
-for (let i = 0; i < moe.length; i++) {
-    for (let j = 0; j < moe.length; j++) {
-        if ((j !== i) && (moe[i] == moe[j])) {
-            console.log(moe[j]);
-        }
+
+var sorted_arr = moe.slice().sort(); 
+var results = [];
+for (var i = 0; i < moe.length - 1; i++) {
+    if (sorted_arr[i + 1] == sorted_arr[i]) {
+        results.push(sorted_arr[i]);
     }
 }
+
+console.log(results);
